d9c004817bd8feaf3b239290342433c210339892
COTE <GrCOTE7@GMail.com>
2018-11-13 14:17:18 +0100

    Add cache

---
+++ application/files/cache/overrides/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/0d949aa07552158c/38b4c9cadc6fcf49.php
@@ -0,0 +1,17 @@
+<?php 
+/* Cachekey: cache/stash_default/overrides.fec9982596129104e7c187cdb69eccf6/ */
+/* Type: array */
+/* Expiration: 2018-11-18T03:48:52+01:00 */
+
+
+
+$loaded = true;
+$expiration = 1542509332;
+
+$data = array();
+
+/* Child Type: object */
+$data['return'] = unserialize(base64_decode('Tzo0MzoiQ29uY3JldGVcQ29yZVxGaWxlc3lzdGVtXEZpbGVMb2NhdG9yXFJlY29yZCI6Njp7czo0OiJmaWxlIjtzOjU5OiJDOi9sYXJhZ29uL3d3dy9sb3NjNS9jb25jcmV0ZS9lbGVtZW50cy9wYW5lbHMvZGFzaGJvYXJkLnBocCI7czozOiJ1cmwiO3M6Mzk6Ii9jb25jcmV0ZS9lbGVtZW50cy9wYW5lbHMvZGFzaGJvYXJkLnBocCI7czo5OiJwa2dIYW5kbGUiO047czo4OiJvdmVycmlkZSI7TjtzOjk6IgAqAGV4aXN0cyI7TjtzOjEzOiIAKgBmaWxlc3lzdGVtIjtPOjMyOiJJbGx1bWluYXRlXEZpbGVzeXN0ZW1cRmlsZXN5c3RlbSI6MDp7fX0='));
+
+/* Child Type: integer */
+$data['createdOn'] = 1542114986;
